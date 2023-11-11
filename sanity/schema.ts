579fa7e61764bb0a442a831ba43d9d592e237f9a
anhtudo97 <anhtudo97@gmail.com>
2023-11-11 15:59:52 +0700

    feat: sanity

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
+import resource from './schemas/resource.schema';
+import resourcePlaylist from './schemas/resource-playlist.schema';
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [resource, resourcePlaylist],
 }
